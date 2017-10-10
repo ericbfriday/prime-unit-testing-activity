0cef7677855e197c4f3ddff8b7063c71907bd8ac
Eric Friday <ericfriday@gmail.com>
2017-10-10 15:28:39 -0500

    added get API for oddEven

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var oddEven = require('./modules/oddEven');
 
 app.use(express.static('src/server/public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
+app.get('/oddEven/:num', function(req,res){
+  console.log('log req . param', req.params.num);
+  var numConvert = parseInt(req.params.num);
+  res.send(oddEven(numConvert));
 });
 
 app.listen(4005, function() {
--- src/server/modules/oddEven.js
+++ src/server/modules/oddEven.js
@@ -2,7 +2,7 @@ function isEvenOrOdd(num) {
   if (typeof num !== 'number'){
     throw new TypeError('/expects a number/');
   } else {
-    var bob = (num % 2 === 0) ? 'even' : 'odd';
+    var bob = (num % 2 === 0) ? 'even' : 'odd'; //if (condition) is true - return first thing which is 'even'
     return bob;
   }
 }
